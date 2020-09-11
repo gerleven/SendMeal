1bf2a916f76d3edb39c0d4cb3bc436198a17668e
gerleven <gerleven@gmail.com>
2020-09-11 20:37:57 -0300

    Termine el xml, falta el .java

--- app/src/main/java/ar/com/leventalgarcia/sendmeal/MainActivity.java
+++ app/src/main/java/ar/com/leventalgarcia/sendmeal/MainActivity.java
@@ -3,12 +3,31 @@ package ar.com.leventalgarcia.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //implements View.OnClickListener
+
+    CheckBox cb1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cb1 = findViewById(R.id.checkBox);
+
+        //Validar contraseñas iguales
+
+        //El numero CCV de la tarjeta es un input numerico el cual estara visible pero deshabilitado si el numero de la tarjeta esta vacio
+
+        //Habilitar sleek bar segun estado del switch:
+        if(cb1.isEnabled()){
+
+        }
+
+
     }
 }
